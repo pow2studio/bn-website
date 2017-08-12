5b8cebd8e840d791b7a393f581e64c4667b59be6
Klaas <klaas@uniq.vision>
2017-08-13 00:18:53 +0200

    Update 2017-08-13_00:18:53

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,44 @@
 $(document).ready(function () {
+
+  var $w = $('.welcome');
+  var imgUrl = '/assets/img/bn_cover.jpg';
+  if (document.body.scrollTop === 0) {
+    var $wh = $('#welcome-heading');
+    $wh.hide();
+    $('.transp').css({ opacity: 0 });
+    var img = $("<img />").on('load', function () {
+      renderBg($w, imgUrl);
+      $(this).remove();
+      new Vivus('bn-icon', { duration: 50 }, function () {
+        $wh.slideToggle(600, function () {
+          $('.transp').animate({ opacity: 1 }, 2400);
+        });
+      });
+    });
+    img.attr('src', imgUrl);
+  } else
+    renderBg($w, imgUrl);
+
+  function renderBg(el, imgUrl) {
+    el.css('background', 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.6)),' +
+      'url(' + imgUrl + ') center center');
+    el.css('background-size', 'auto, cover');
+  }
+
   $('.philosophy .cover').parallax({
-    imageSrc: '/assets/img/cover_areas.jpg',
+    imageSrc: '/assets/img/cover_areas_d.jpg',
     naturalWidth: 1280,
     naturalHeight: 400,
     speed: 0.8
   });
   $('.team .cover').parallax({
-    imageSrc: '/assets/img/cover_team.jpg',
+    imageSrc: '/assets/img/cover_team_d.jpg',
     naturalWidth: 1280,
     naturalHeight: 400,
     speed: 0.8
   });
   $('.contact .cover').parallax({
-    imageSrc: '/assets/img/cover_contact.jpg',
+    imageSrc: '/assets/img/cover_contact_d.jpg',
     naturalWidth: 1280,
     naturalHeight: 400,
     speed: 0.8
@@ -20,11 +46,15 @@ $(document).ready(function () {
   $('.fix-height').css('min-height', function () {
     return $(this).height()
   });
-  var mixer = mixitup('.members', {
+  mixitup('.members', {
     callbacks: {
       onMixEnd: function () {
         $(window).trigger('resize').trigger('scroll');
       }
     }
+  });
+  var nav = $('.navbar-collapse');
+  nav.on('click', 'a:not([data-toggle])', null, function () {
+    nav.collapse('hide');
   });
 });
